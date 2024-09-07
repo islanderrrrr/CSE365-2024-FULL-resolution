076d8a3d767e63dae2e4c2e96f61005fe7a58883
islanderrrrr <154747088+islanderrrrr@users.noreply.github.com>
2024-09-07 15:22:52 +0800

    Update level57.py

--- program_interaction/level57.py
+++ program_interaction/level57.py
@@ -1,1 +1,8 @@
+#!/usr/bin/python
+import subprocess
+p1 = subprocess.Popen(["/challenge/run"], stdout=subprocess.PIPE)#只能用Popen，其他会失败
+p2 = subprocess.Popen(["rev"], stdin=p1.stdout, stdout=subprocess.PIPE)
+output = p2.communicate()[0]
+print(output)
 
+#没啥说的，就是将/challenge/run 的执行结果，作为输入进rev指令
